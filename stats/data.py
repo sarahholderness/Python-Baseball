034f82acafa83dbdad787d1c131454d44c26813f
sarah <sarah-holderness@pluralsight.com>
2019-02-01 15:14:32 -0500

    Completed 2.10-12

--- stats/data.py
+++ stats/data.py
@@ -15,3 +15,6 @@ for game_file in game_files:
 games = pd.concat(game_frames)
 games.loc[games['multi5'] == '??', 'multi5'] = ''
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+identifiers = identifiers.fillna(method='ffill')
+identifiers.columns = ['game_id', 'year']
+games = pd.concat([games, identifiers], axis=1, sort=False)

255e74d7c64e5a66bc008bd0f8c3a4b3906a484b
sarah <sarah-holderness@pluralsight.com>
2019-02-05 11:31:13 -0500

    Completed Module 3

--- stats/attendance.py
+++ stats/attendance.py
@@ -11,4 +11,8 @@ attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:,
 'attendance'])
 
 attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
+plt.xlabel('Year')
+plt.ylabel('Attendance')
+plt.axhline( y=attendance['attendance'].mean(), label='Mean', linestyle='--',
+color='green' )
 plt.show()

3b5e347f8cde27b8e09a77f549d08479700bec92
sarah <sarah-holderness@pluralsight.com>
2019-02-05 16:56:45 -0500

    Completed module 5

--- stats/offense.py
+++ stats/offense.py
@@ -17,3 +17,16 @@ replacements = {r'^S(.*)': 'single', r'^D(.*)': 'double',
                 r'^T(.*)': 'triple', r'^HR(.*)': 'hr'}
 
 hit_type = hits['event'].replace(replacements, regex=True)
+
+hits = hits.assign(hit_type=hit_type)
+
+hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
+
+hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
+
+hits = hits.sort_values(['inning', 'hit_type'])
+
+hits = hits.pivot(index='inning', columns='hit_type', values='count')
+
+hits.plot.bar(stacked=True)
+plt.show()

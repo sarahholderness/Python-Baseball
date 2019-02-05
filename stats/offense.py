8b0977ba7b57912b9998ca5160d04b6ba490131e
sarah <sarah-holderness@pluralsight.com>
2019-02-05 16:21:16 -0500

    Completed 5.2-5

--- stats/offense.py
+++ stats/offense.py
@@ -7,3 +7,13 @@ plays = games[games['type']== 'play']
 
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches',
 'event', 'game_id', 'year']
+
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'),
+['inning', 'event']]
+
+hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
+
+replacements = {r'^S(.*)': 'single', r'^D(.*)': 'double',
+                r'^T(.*)': 'triple', r'^HR(.*)': 'hr'}
+
+hit_type = hits['event'].replace(replacements, regex=True)

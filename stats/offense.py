c3e606cda24f09bcc1b1f10b3f51646ae5ccfb39
sarah <sarah-holderness@pluralsight.com>
2019-02-05 12:10:24 -0500

    Completed 5.1

--- stats/offense.py
+++ stats/offense.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import games
+
+plays = games[games['type']== 'play']
+
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches',
+'event', 'game_id', 'year']
